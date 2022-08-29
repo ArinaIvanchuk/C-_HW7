af13be8fb82726e847a23ed1d766b0fb5184a650
Arina <arina_ivanchyk@mail.ru>
2022-08-29 22:09:31 +0300

    add folder 3

---
+++ Task3/Program.cs
@@ -0,0 +1,53 @@
+﻿//Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+int InputData()
+{
+    Console.WriteLine("input data");
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int[,] Matrix(int line, int colum, int minRand = -100, int maxRand = 100)
+{
+    int[,] matrix = new int[line, colum];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(minRand, maxRand);
+        }
+    }
+    return matrix;
+}
+void ShowArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int line = InputData();
+int colum = InputData();
+int [] sum = new int[colum];
+int[,] array = Matrix(line, colum);
+ShowArray(array);
+for (int i=0; i<line; i++)
+{
+    for(int j=0; j<colum; j++)
+    {
+        sum[i] += array[j, i];
+    }
+}
+
+for (int i=0; i<line; i++)
+{
+    double mid=0;
+    mid = (double)sum[i] / colum;
+    Console.Write($"mid = {mid:f1} ");
+}
+
